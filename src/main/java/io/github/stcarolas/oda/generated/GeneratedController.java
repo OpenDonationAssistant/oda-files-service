011b249c882ba65a4b8f521f60a6b433522f12b4
stCarolas <stcarolas@gmail.com>
2024-12-17 20:32:04 +0300

    some fixes for generated files

--- src/main/java/io/github/stcarolas/oda/generated/GeneratedController.java
+++ src/main/java/io/github/stcarolas/oda/generated/GeneratedController.java
@@ -4,6 +4,8 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import java.util.Base64;
 
 @Controller("/generated")
@@ -17,10 +19,10 @@ public class GeneratedController {
     this.token = token;
   }
 
-  @Get(value = "/{name}", produces = { MediaType.APPLICATION_OCTET_STREAM })
+  @Get(value = "{name}", produces = { MediaType.APPLICATION_OCTET_STREAM })
+  @Secured(SecurityRule.IS_ANONYMOUS)
   public byte[] get(@PathVariable String name) {
     var result = artClient.operations(token, name);
     return Base64.getDecoder().decode(result.getResponse().image());
   }
-
 }
