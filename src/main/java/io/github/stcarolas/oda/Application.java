282fc36ee08b9defb996f1c35d4b687c657d503e
stCarolas <stcarolas@gmail.com>
2025-12-13 02:27:48 +0300

    add missing conf

--- src/main/java/io/github/stcarolas/oda/Application.java
+++ src/main/java/io/github/stcarolas/oda/Application.java
@@ -1,14 +1,29 @@
 package io.github.stcarolas.oda;
 
+import org.jspecify.annotations.NonNull;
+
 import io.github.opendonationassistant.rabbit.RabbitConfiguration;
+import io.micronaut.context.ApplicationContextBuilder;
+import io.micronaut.context.ApplicationContextConfigurer;
+import io.micronaut.context.annotation.ContextConfigurer;
 import io.micronaut.rabbitmq.connect.ChannelInitializer;
 import io.micronaut.runtime.Micronaut;
 import jakarta.inject.Singleton;
 
 public class Application {
 
+  @ContextConfigurer
+  public static class Configurer implements ApplicationContextConfigurer {
+
+    @Override
+    public void configure(@NonNull ApplicationContextBuilder builder) {
+      builder.defaultEnvironments("standalone");
+    }
+  }
+
+
   public static void main(String[] args) {
-    Micronaut.run(Application.class, args);
+    Micronaut.build(args).banner(false).classes(Application.class).start();
   }
 
   @Singleton
