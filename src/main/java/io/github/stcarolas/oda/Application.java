41c240b9bc43850d38a9b639deca4e6caa43e1c7
stCarolas <stcarolas@gmail.com>
2026-05-18 00:30:48 +0300

    fix missing rabbit conf

--- src/main/java/io/github/stcarolas/oda/Application.java
+++ src/main/java/io/github/stcarolas/oda/Application.java
@@ -6,6 +6,7 @@ import io.github.opendonationassistant.rabbit.Queue;
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.ApplicationContextConfigurer;
 import io.micronaut.context.annotation.ContextConfigurer;
+import io.micronaut.context.annotation.Factory;
 import io.micronaut.rabbitmq.connect.ChannelInitializer;
 import io.micronaut.runtime.Micronaut;
 import jakarta.inject.Singleton;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 import org.jspecify.annotations.NonNull;
 
+@Factory
 public class Application {
 
   @ContextConfigurer
